166113ce670698e9c6d75486a9cfaa483e3ec078
Brian Lalor <blalor@bravo5.org>
2010-10-31 14:45:43 +0000

    rewriting dispatcher to use new XBee library

--- dispatcher.py
+++ dispatcher.py
@@ -22,49 +22,17 @@ import Queue
 
 import xbee, serial
 import struct
+import cPickle as pickle
 
-class XBeeReaderThread(threading.Thread):
-    """
-    XBee thread.  Reads packets from 
-    """
-    
-    def __init__(self, serial_dev, baud, server):
-        threading.Thread.__init__(self, name = "XBee")
-        
-        self.device = serial_dev
-        self.baud = baud
-        self.server = server
-        self.__shutdown = False
-        
-    
-    
-    def run(self):
-        ser = serial.Serial(self.device, self.baud)
-        
-        try:
-            while not self.__shutdown:
-                packet = xbee.xbee.find_packet(ser)
-                
-                if packet:
-                    self.server.dispatch_packet(packet)
-        finally:
-            ser.close()
-    
-    
-    def shutdown(self):
-        self.__shutdown = True
-    
-
-
-class XBeeDispatcher(SocketServer.ThreadingUnixStreamServer):
+# class XBeeDispatcher(SocketServer.ThreadingUnixStreamServer):
+class XBeeDispatcher(SocketServer.ThreadingTCPServer):
     daemon_threads = True
     allow_reuse_address = True
     
-    def __init__(self, address, serial_device, serial_baud):
-        self.serial_device = serial_device
-        self.serial_baud = serial_baud
-        
-        SocketServer.ThreadingUnixStreamServer.__init__(self, address, XBeeRequestHandler)
+    def __init__(self, address, ser):
+        self.serial = ser
+        # SocketServer.ThreadingUnixStreamServer.__init__(self, address, XBeeRequestHandler)
+        SocketServer.TCPServer.__init__(self, address, XBeeRequestHandler)
     
     def server_activate(self):
         self.__shutdown = False
@@ -72,33 +40,47 @@ class XBeeDispatcher(SocketServer.ThreadingUnixStreamServer):
         self.__active_clients = []
         self.__active_clients_lock = threading.RLock()
         
-        SocketServer.ThreadingUnixStreamServer.server_activate(self)
+        self.__serial_write_lock = threading.RLock()
+        
+        # SocketServer.ThreadingUnixStreamServer.server_activate(self)
+        SocketServer.TCPServer.server_activate(self)
+        
+        print 'request queue size:', self.request_queue_size
         
         ## start XBee communication thread here
-        self.xbee_thread = XBeeReaderThread(self.serial_device, self.serial_baud, self)
-        self.xbee_thread.start()
+        self.xbee_thread = xbee.XBee(self.serial, callback = self.dispatch_packet)
+        
+    
     
     def serve_forever(self):
         while not self.__shutdown:
             self.handle_request()
     
+    
     def server_close(self):
         self.__shutdown = True
         
-        SocketServer.ThreadingUnixStreamServer.server_close(self)
+        # SocketServer.ThreadingUnixStreamServer.server_close(self)
+        SocketServer.TCPServer.server_close(self)
         
         ## close down XBee comm thread
-        self.xbee_thread.shutdown()
+        self.xbee_thread.halt()
+        self.serial.close()
+        del self.xbee_thread
+        
+    
     
     def add_active_client(self, client):
         with self.__active_clients_lock:
             if client not in self.__active_clients:
                 self.__active_clients.append(client)
     
+    
     def remove_active_client(self, client):
         with self.__active_clients_lock:
             if client in self.__active_clients:
                 self.__active_clients.remove(client)
+    
     
     def dispatch_packet(self, packet):
         found_clients = False
@@ -112,6 +94,12 @@ class XBeeDispatcher(SocketServer.ThreadingUnixStreamServer):
         if not found_clients:
             print >>sys.stderr, "no active clients"
     
+    
+    def send_packet(self, name, **kwargs):
+        with self.__serial_write_lock:
+            self.xbee_thread.send(name, **kwargs)
+        
+    
 
 
 class XBeeRequestHandler(SocketServer.BaseRequestHandler):
@@ -121,6 +109,7 @@ class XBeeRequestHandler(SocketServer.BaseRequestHandler):
         except Queue.Full:
             print >>sys.stderr, "queue is full!"
     
+    
     def setup(self):
         self.packet_queue = Queue.Queue()
         
@@ -129,26 +118,49 @@ class XBeeRequestHandler(SocketServer.BaseRequestHandler):
         
         ## think this might only work with TCP sockets
         self.request.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+        print >>sys.stderr, "new connection"
+    
     
     def handle(self):
         ## we *must* ensure that finish() gets called, which only happens when
         ## handle() returns successfully.
         
-        ## at this time, I'm only sending data *to* the clients
-        self.request.shutdown(socket.SHUT_RD)
+        header_len = struct.calcsize('I')
         
         try:
             connection_alive = True
             
             while connection_alive:
+                # enable short timeout for receiving data
+                self.request.settimeout(0.25)
+                
+                try:
+                    header_dat = self.request.recv(header_len)
+                    if len(header_dat) == 0:
+                        print 'peer closed the connection'
+                        connection_alive = False
+                        continue
+                    elif len(header_dat) != header_len:
+                        print '%d bytes for header, need %d: %s' % (len(header_dat), header_len, " ".join(['%.2x' % (ord(c),) for c in header_dat]))
+                    else:
+                        data_len = struct.unpack('I', header_dat)[0]
+                        packet = pickle.loads(self.request.recv(data_len))
+                        self.server.send_packet(packet[0], **packet[1])
+                except socket.timeout:
+                    # timeout reading data
+                    pass
+                
+                # disable the timeout for sending data
+                self.request.settimeout(None)
+                
                 try:
                     # pull a packet from the queue, with a blocking timeout
                     packet = self.packet_queue.get(True, 1)
                     
-                    length = len(packet)
+                    data = pickle.dumps(packet)
                     
-                    self.request.send(struct.pack('BH', xbee.xbee.START_IOPACKET, length))
-                    self.request.sendall(packet)
+                    self.request.send(struct.pack('I', len(data)))
+                    self.request.sendall(data)
                 except Queue.Empty:
                     pass
                 except socket.error:
@@ -171,13 +183,19 @@ class XBeeRequestHandler(SocketServer.BaseRequestHandler):
         self.server.remove_active_client(self)
         
         ## close the socket
+        self.request.shutdown(socket.SHUT_RDWR)
         self.request.close()
     
 
 
 def main():
-    server = XBeeDispatcher("socket", sys.argv[1], int(sys.argv[2]))
-
+    server = XBeeDispatcher(
+        # "socket",
+        ('', 9999),
+        serial.Serial(port=sys.argv[1], baudrate=int(sys.argv[2]),rtscts=True)
+    )
+    
+    
     # The signals SIGKILL and SIGSTOP cannot be caught, blocked, or ignored.
     signal.signal(signal.SIGHUP, signal.SIG_IGN)
     signal.signal(signal.SIGQUIT, lambda signum, frame: server.server_close())
