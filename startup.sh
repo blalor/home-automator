1a8ed4bed8a8bed8e1301931370583e992c5be1a
Brian Lalor <blalor@bravo5.org>
2010-11-06 11:35:45 +0000

    finalizing startup script

--- startup.sh
+++ startup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -ex
 
 cd $(dirname $0)
-./dispatcher.py /dev/ttyUSB0 38400
+./dispatcher.py /dev/ttyUSB0 115200
 sleep 5
 ./environmental_node_consumer.py
-./fuel_oil_tank_consumer.py
 ./furnace_consumer.py
 ./power_consumer.py
 ./xbee_lt_sensor.py
+./fuel_oil_tank_consumer.py
