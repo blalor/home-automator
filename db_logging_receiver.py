2b23ac99fcd7c850794567f476a5ca0c68bada20
Brian Lalor <blalor@bravo5.org>
2011-11-03 00:06:52 -0400

    well, at least it's a class, now...

--- db_logging_receiver.py
+++ db_logging_receiver.py
@@ -16,135 +16,147 @@ import pika
 import cPickle as pickle
 
 import sqlite3
+import copy
 
-ROUTING_KEYS = (
-    'electric_meter',
-    'light.*',
-    'temperature.*',
-    'humidity.*',
-    'oil_tank',
-    'furnace',
-)
-
-_logger = logging.getLogger("DBLogger")
-
-connection = None
-channel = None
-dbc = None
-
-def on_connected(conn):
-    global connection
+class DBLogger(object):
+    """docstring for DBLogger"""
     
-    connection = conn
+    ROUTING_KEYS = (
+        'electric_meter',
+        'light.*',
+        'temperature.*',
+        'humidity.*',
+        'oil_tank',
+        'furnace',
+    )
     
-    _logger.debug("connection opened")
-    connection.channel(on_channel_open)
-
-
-def on_channel_open(chan):
-    global channel
-    
-    channel = chan
-    
-    _logger.debug("channel opened: %s", channel)
-    
-    # create new queue exclusively for us
-    channel.queue_declare(queue = 'db_inserts', callback = on_queue_declared)
-
-
-def on_queue_declared(frame):
-    _logger.debug("queue declared: %s", frame)
-    
-    channel.basic_consume(on_receive_message,
-                          queue = frame.method.queue)
-    
-    for rk in ROUTING_KEYS:
-        channel.queue_bind(lambda *a: queue_did_bind(frame.method.queue, rk, *a),
-                           exchange = 'sensor_data',
-                           queue = frame.method.queue,
-                           routing_key = rk)
-    
-
-
-def queue_did_bind(queue_name, rk, *args):
-    _logger.debug("queue %s did bind with routing key %s: %s", queue_name, rk, args)
-
-
-# {{{ on_receive_message
-def on_receive_message(channel, method, properties, body):
-    global dbc
-    
-    frame = pickle.loads(body)
-    
-    query = None
-    data = ()
-    
-    if method.routing_key == 'electric_meter':
-        query = "insert into power (ts_utc, clamp1, clamp2) values (?, ?, ?)"
-        data = (
-            time.mktime(frame['timestamp'].timetuple()),
-            frame['clamp1_amps'],
-            frame['clamp2_amps'],
-        )
-    
-    elif method.routing_key.startswith('temperature.'):
-        query = "insert into temperature (ts_utc, node_id, temp_C) values (?, ?, ?)"
-        data = (
-            time.mktime(frame['timestamp'].timetuple()),
-            frame['node_id'],
-            frame['temp_C'],
-        )
-    
-    elif method.routing_key.startswith('light.'):
-        query = "insert into light (ts_utc, node_id, light_val) values (?, ?, ?)"
-        data = (
-            time.mktime(frame['timestamp'].timetuple()),
-            frame['node_id'],
-            frame['light'],
-        )
-    
-    elif method.routing_key.startswith('humidity.'):
-        query = "insert into humidity (ts_utc, node_id, rel_humid) values (?, ?, ?)"
-        data = (
-            time.mktime(frame['timestamp'].timetuple()),
-            frame['node_id'],
-            frame['rel_humid'],
-        )
-    
-    elif method.routing_key == 'oil_tank':
-        query = "insert into oil_tank (ts_utc, height) values (?, ?)"
-        data = (
-            time.mktime(frame['timestamp'].timetuple()),
-            frame['height'],
-        )
-    
-    elif method.routing_key == 'furnace':
-        query = "insert into furnace (ts_utc, zone_active) values (?, ?)"
-        data = (
-            time.mktime(frame['timestamp'].timetuple()),
-            frame['zone_active']
-        )
-    
-    else:
-        _logger.critical("UNKNOWN ROUTING KEY %s", method.routing_key)
-    
-    if query and data:
-        _logger.debug("%s %s", frame['timestamp'].isoformat(), str((query, data)))
+    # {{{ __init__
+    def __init__(self, host):
+        super(DBLogger, self).__init__()
         
-        can_ack = False
+        self._logger = logging.getLogger(self.__class__.__name__)
+        
+        parameters = pika.ConnectionParameters(host = host)
+        self.connection = pika.SelectConnection(parameters, self.on_connected)
+        
+        self.channel = None
+        self.dbc = sqlite3.connect(os.path.abspath(os.path.dirname(__file__)) + '/sensors.db',
+                                   isolation_level = None,
+                                   timeout = 5)
+        
+        
+    
+    # }}}
+    
+    def on_connected(self, conn):
+        self._logger.debug("connection opened")
+        self.connection.channel(self.on_channel_open)
+    
+    
+    def on_channel_open(self, chan):
+        self.channel = chan
+        
+        self._logger.debug("channel opened: %s", self.channel)
+        
+        # create new queue 
+        self.channel.queue_declare(queue = 'db_inserts', callback = self.on_queue_declared)
+    
+    
+    def on_queue_declared(self, frame):
+        self._logger.debug("queue declared: %s", frame)
+        
+        self.channel.basic_consume(self.on_receive_message,
+                                   queue = frame.method.queue)
+        
+        for rk in self.ROUTING_KEYS:
+            self.channel.queue_bind(exchange = 'sensor_data',
+                                    queue = frame.method.queue,
+                                    routing_key = rk)
+            
+    
+    
+    # {{{ on_receive_message
+    def on_receive_message(self, channel, method, properties, body):
+        frame = pickle.loads(body)
+        
+        query = None
+        data = ()
+        
+        if method.routing_key == 'electric_meter':
+            query = "insert into power (ts_utc, clamp1, clamp2) values (?, ?, ?)"
+            data = (
+                time.mktime(frame['timestamp'].timetuple()),
+                frame['clamp1_amps'],
+                frame['clamp2_amps'],
+            )
+        
+        elif method.routing_key.startswith('temperature.'):
+            query = "insert into temperature (ts_utc, node_id, temp_C) values (?, ?, ?)"
+            data = (
+                time.mktime(frame['timestamp'].timetuple()),
+                frame['node_id'],
+                frame['temp_C'],
+            )
+        
+        elif method.routing_key.startswith('light.'):
+            query = "insert into light (ts_utc, node_id, light_val) values (?, ?, ?)"
+            data = (
+                time.mktime(frame['timestamp'].timetuple()),
+                frame['node_id'],
+                frame['light'],
+            )
+        
+        elif method.routing_key.startswith('humidity.'):
+            query = "insert into humidity (ts_utc, node_id, rel_humid) values (?, ?, ?)"
+            data = (
+                time.mktime(frame['timestamp'].timetuple()),
+                frame['node_id'],
+                frame['rel_humid'],
+            )
+        
+        elif method.routing_key == 'oil_tank':
+            query = "insert into oil_tank (ts_utc, height) values (?, ?)"
+            data = (
+                time.mktime(frame['timestamp'].timetuple()),
+                frame['height'],
+            )
+        
+        elif method.routing_key == 'furnace':
+            query = "insert into furnace (ts_utc, zone_active) values (?, ?)"
+            data = (
+                time.mktime(frame['timestamp'].timetuple()),
+                frame['zone_active']
+            )
+        
+        else:
+            self._logger.critical("UNKNOWN ROUTING KEY %s", method.routing_key)
+        
+        if query and data:
+            self._logger.debug("%s %s", frame['timestamp'].isoformat(), str((query, data)))
+            
+            can_ack = False
+            try:
+                self.dbc.execute(query, data)
+                can_ack = True
+            except sqlite3.IntegrityError:
+                self._logger.error("IntegrityError executing query %s", str((query, data)), exc_info = True)
+                can_ack = True
+            except:
+                self._logger.critical("error executing query %s", str((query, data)), exc_info = True)
+            
+            if can_ack:
+                self.channel.basic_ack(delivery_tag = method.delivery_tag)
+    
+    # }}}
+    
+    def process_forever(self):
         try:
-            dbc.execute(query, data)
-            can_ack = True
-        except sqlite3.IntegrityError:
-            _logger.error("IntegrityError executing query %s", str((query, data)), exc_info = True)
-            can_ack = True
-        except:
-            _logger.critical("error executing query %s", str((query, data)), exc_info = True)
+            self.connection.ioloop.start()
+        finally:
+            self._logger.info("cleaning up")
+            self.connection.close()
+            self.connection.ioloop.start()
         
-        if can_ack:
-            channel.basic_ack(delivery_tag = method.delivery_tag)
-
-# }}}
 
 
 def main():
@@ -158,19 +170,14 @@ def main():
     
     log_config.init_logging_stdout()
     
-    global dbc
-    dbc = sqlite3.connect(basedir + '/sensors.db',
-                          isolation_level = None,
-                          timeout = 5)
-    
-    parameters = pika.ConnectionParameters(host = 'pepe')
-    connection = pika.SelectConnection(parameters, on_connected)
+    dbl = DBLogger('pepe')
     
     try:
-        connection.ioloop.start()
+        dbl.process_forever()
     except KeyboardInterrupt:
-        connection.close()
-        connection.ioloop.start()
+        pass
+    except:
+        logging.getLogger().critical("uncaught exception", exc_info = True)
     finally:
         log_config.shutdown()
     
